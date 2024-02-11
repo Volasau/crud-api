f825e64dea2cadd61e323a75e872d1b54e74b7d0
Ryhor Volsasu <g.volosov@email.com>
2024-02-12 01:07:27 +0400

    feat: add delete function and catch in functions for 500 error

--- src/router/functions.ts
+++ src/router/functions.ts
@@ -19,104 +19,148 @@ export function getAllUsers(req: IncomingMessage, res: ServerResponse) {
 }
 
 export function addUser(req: IncomingMessage, res: ServerResponse) {
-  let body = '';
+  try {
+    let body = '';
 
-  req.on('data', (chunk) => {
-    body += chunk.toString();
-  });
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
 
-  req.on('end', () => {
-    try {
-      const userData = JSON.parse(body);
+    req.on('end', () => {
+      try {
+        const userData = JSON.parse(body);
 
-      if (
-        !userData.username ||
-        !userData.age ||
-        !userData.hobbies ||
-        !Array.isArray(userData.hobbies)
-      ) {
+        if (
+          !userData.username ||
+          !userData.age ||
+          !userData.hobbies ||
+          !Array.isArray(userData.hobbies)
+        ) {
+          res.writeHead(ICodes.BAD_REQUEST, { 'Content-Type': 'text/plain' });
+          res.end('Missing required fields');
+          return;
+        }
+
+        const newUser: IUser = {
+          id: uuidv4(),
+          username: userData.username,
+          age: userData.age,
+          hobbies: userData.hobbies,
+        };
+
+        users.push(newUser);
+
+        res.writeHead(ICodes.CREATED, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(newUser));
+      } catch (error) {
         res.writeHead(ICodes.BAD_REQUEST, { 'Content-Type': 'text/plain' });
-        res.end('Missing required fields');
-        return;
+        res.end('Invalid JSON');
       }
-
-      const newUser: IUser = {
-        id: uuidv4(),
-        username: userData.username,
-        age: userData.age,
-        hobbies: userData.hobbies,
-      };
-
-      users.push(newUser);
-
-      res.writeHead(ICodes.CREATED, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(newUser));
-    } catch (error) {
-      res.writeHead(ICodes.BAD_REQUEST, { 'Content-Type': 'text/plain' });
-      res.end('Invalid JSON');
-    }
-  });
+    });
+  } catch (error) {
+    res.writeHead(ICodes.SERVER_ERROR, { 'Content-Type': 'text/plain' });
+    res.end('500 Internal Server Error');
+  }
 }
 
 export function getUser(req: IncomingMessage, res: ServerResponse) {
-  const urlParts = req.url.split('/');
-  const userId = urlParts[urlParts.length - 1];
-  if (!uuidValidate(userId)) {
-    res.writeHead(ICodes.BAD_REQUEST, { 'Content-Type': 'text/plain' });
-    res.end('Invalid userId');
-    return;
+  try {
+    const urlParts = req.url.split('/');
+    const userId = urlParts[urlParts.length - 1];
+    if (!uuidValidate(userId)) {
+      res.writeHead(ICodes.BAD_REQUEST, { 'Content-Type': 'text/plain' });
+      res.end('Invalid userId');
+      return;
+    }
+    const user = users.find((user) => user.id === userId);
+    if (!user) {
+      res.writeHead(ICodes.NOT_FOUND, { 'Content-Type': 'text/plain' });
+      res.end('User not found');
+      return;
+    }
+    res.writeHead(ICodes.OK, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(user));
+  } catch (error) {
+    res.writeHead(ICodes.SERVER_ERROR, { 'Content-Type': 'text/plain' });
+    res.end('500 Internal Server Error');
   }
-  const user = users.find((user) => user.id === userId);
-  if (!user) {
-    res.writeHead(ICodes.NOT_FOUND, { 'Content-Type': 'text/plain' });
-    res.end('User not found');
-    return;
-  }
-  res.writeHead(ICodes.OK, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(user));
 }
 
 export function updateUser(req: IncomingMessage, res: ServerResponse) {
-  const urlParts = req.url.split('/');
-  const userId = urlParts[urlParts.length - 1];
-  if (!uuidValidate(userId)) {
-    res.writeHead(ICodes.BAD_REQUEST, { 'Content-Type': 'text/plain' });
-    res.end('Invalid userId');
-    return;
-  }
-  const user = users.find((user) => user.id === userId);
-  if (!user) {
-    res.writeHead(ICodes.NOT_FOUND, { 'Content-Type': 'text/plain' });
-    res.end('User not found');
-    return;
-  }
-  const userIndex = users.findIndex((user) => user.id === userId);
-  let body = '';
-  req.on('data', (chunk) => {
-    body += chunk.toString();
-  });
-  req.on('end', () => {
-    try {
-      const userData: Partial<IUser> = JSON.parse(body);
-      if (
-        !userData.username ||
-        !userData.age ||
-        !userData.hobbies ||
-        !Array.isArray(userData.hobbies)
-      ) {
-        res.writeHead(ICodes.BAD_REQUEST, { 'Content-Type': 'text/plain' });
-        res.end('Missing required fields');
-        return;
-      }
-
-      const updatedUser = { ...users[userIndex], ...userData };
-      users[userIndex] = updatedUser;
-
-      res.writeHead(ICodes.OK, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(updatedUser));
-    } catch (error) {
+  try {
+    const urlParts = req.url.split('/');
+    const userId = urlParts[urlParts.length - 1];
+    if (!uuidValidate(userId)) {
       res.writeHead(ICodes.BAD_REQUEST, { 'Content-Type': 'text/plain' });
-      res.end('Invalid JSON');
+      res.end('Invalid userId');
+      return;
     }
-  });
+
+    const user = users.find((user) => user.id === userId);
+    if (!user) {
+      res.writeHead(ICodes.NOT_FOUND, { 'Content-Type': 'text/plain' });
+      res.end('User not found');
+      return;
+    }
+
+    const userIndex = users.findIndex((user) => user.id === userId);
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      try {
+        const userData: Partial<IUser> = JSON.parse(body);
+        if (
+          !userData.username ||
+          !userData.age ||
+          !userData.hobbies ||
+          !Array.isArray(userData.hobbies)
+        ) {
+          res.writeHead(ICodes.BAD_REQUEST, { 'Content-Type': 'text/plain' });
+          res.end('Missing required fields');
+          return;
+        }
+
+        const updatedUser = { ...users[userIndex], ...userData };
+        users[userIndex] = updatedUser;
+
+        res.writeHead(ICodes.OK, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(updatedUser));
+      } catch (error) {
+        res.writeHead(ICodes.BAD_REQUEST, { 'Content-Type': 'text/plain' });
+        res.end('Invalid JSON');
+      }
+    });
+  } catch (error) {
+    res.writeHead(ICodes.SERVER_ERROR, { 'Content-Type': 'text/plain' });
+    res.end('500 Internal Server Error');
+  }
+}
+
+export function deleteUser(req: IncomingMessage, res: ServerResponse) {
+  try {
+    const urlParts = req.url.split('/');
+    const userId = urlParts[urlParts.length - 1];
+    if (!uuidValidate(userId)) {
+      res.writeHead(ICodes.BAD_REQUEST, { 'Content-Type': 'text/plain' });
+      res.end('Invalid userId');
+      return;
+    }
+
+    const user = users.find((user) => user.id === userId);
+    if (!user) {
+      res.writeHead(ICodes.NOT_FOUND, { 'Content-Type': 'text/plain' });
+      res.end('User not found');
+      return;
+    }
+    const userIndex = users.findIndex((user) => user.id === userId);
+    users.splice(userIndex, 1);
+
+    res.writeHead(ICodes.FOUND_DELETED, { 'Content-Type': 'text/plain' });
+    res.end('User deleted');
+  } catch (error) {
+    res.writeHead(ICodes.SERVER_ERROR, { 'Content-Type': 'text/plain' });
+    res.end('500 Internal Server Error');
+  }
 }
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,20 +1,31 @@
 import { IncomingMessage, ServerResponse } from 'http';
-import { addUser, getAllUsers, getUser, updateUser } from './functions';
+import {
+  addUser,
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+} from './functions';
+import { ICodes } from '../interface/Codes';
 
 export function handleRequest(req: IncomingMessage, res: ServerResponse) {
-  if (
-    req.method === 'GET' &&
-    (req.url === '/api/users' || req.url === '/api/users/')
-  ) {
-    getAllUsers(req, res);
-  } else if (req.method === 'POST' && req.url === '/api/users') {
-    addUser(req, res);
-  } else if (req.method === 'GET' && req.url.startsWith('/api/users/')) {
-    getUser(req, res);
-  } else if (req.method === 'PUT' && req.url.startsWith('/api/users/')) {
-    updateUser(req, res);
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404 Not Found');
+  try {
+    if (req.method === 'GET' && req.url === '/api/users') {
+      getAllUsers(req, res);
+    } else if (req.method === 'POST' && req.url === '/api/users') {
+      addUser(req, res);
+    } else if (req.method === 'GET' && req.url.startsWith('/api/users/')) {
+      getUser(req, res);
+    } else if (req.method === 'PUT' && req.url.startsWith('/api/users/')) {
+      updateUser(req, res);
+    } else if (req.method === 'DELETE' && req.url.startsWith('/api/users/')) {
+      deleteUser(req, res);
+    } else {
+      res.writeHead(ICodes.NOT_FOUND, { 'Content-Type': 'text/plain' });
+      res.end('404 Not Found');
+    }
+  } catch (error) {
+    res.writeHead(ICodes.SERVER_ERROR, { 'Content-Type': 'text/plain' });
+    res.end('500 Internal Server Error');
   }
 }
