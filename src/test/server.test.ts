46b50172c44ad665880f838a2a55878743c6ec16
Ryhor Volsasu <g.volosov@email.com>
2024-02-12 18:11:02 +0400

    feat: add test for add new users for POST method

--- src/test/server.test.ts
+++ src/test/server.test.ts
@@ -4,8 +4,6 @@ import { IUser } from '../interface/User';
 import { ICodes } from '../interface/Codes';
 
 describe('Server', () => {
-  const users: IUser[] = [];
-
   test('should respond with status code 200', async () => {
     const response = await request(server).get('/api/users');
     expect(response.status).toBe(200);
@@ -19,6 +17,10 @@ describe('Server', () => {
     const response = await request(server).get('/api/users');
     expect(response.headers['content-type']).toMatch(/json/);
   });
+});
+
+describe('Different methods', () => {
+  const users: IUser[] = [];
 
   test('should return empty array', async () => {
     const response = await request(server)
@@ -29,5 +31,25 @@ describe('Server', () => {
     expect(response.headers['content-type']).toEqual('application/json');
     expect(response.body).toBeDefined();
     expect(response.body.length).toEqual(users.length);
+  });
+  test('should create  new user', async () => {
+    const newUser = {
+      username: 'Ryhor',
+      age: 40,
+      hobbies: ['bla bla', 'tuk tuk'],
+    };
+
+    const res = await request(server)
+      .post('/api/users')
+      .send(newUser)
+      .set('Accept', 'application/json');
+
+    expect(res.statusCode).toEqual(ICodes.CREATED);
+    expect(res.headers['content-type']).toEqual('application/json');
+    expect(res.body).toBeDefined();
+    expect(res.body.id).toBeDefined();
+    expect(res.body.username).toEqual(newUser.username);
+    expect(res.body.age).toEqual(newUser.age);
+    expect(res.body.hobbies).toEqual(newUser.hobbies);
   });
 });
