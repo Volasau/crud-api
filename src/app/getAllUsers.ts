615370f612efe2bd40c3f109a7bf2a178d2493b8
Ryhor Volsasu <g.volosov@email.com>
2024-02-12 15:18:36 +0400

    feat: add tru-catch for getAllUsers function

--- src/app/getAllUsers.ts
+++ src/app/getAllUsers.ts
@@ -3,6 +3,11 @@ import { users } from '../data/database';
 import { ICodes } from '../interface/Codes';
 
 export function getAllUsers(req: IncomingMessage, res: ServerResponse) {
-  res.writeHead(ICodes.OK, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(users));
+  try {
+    res.writeHead(ICodes.OK, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(users));
+  } catch (error) {
+    res.writeHead(ICodes.SERVER_ERROR, { 'Content-Type': 'text/plain' });
+    res.end('500 Internal Server Error');
+  }
 }
