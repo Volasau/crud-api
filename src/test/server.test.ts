106c5c1ee322d1dcf84c2bd264f55bc128fcf8c3
Ryhor Volsasu <g.volosov@email.com>
2024-02-12 17:57:20 +0400

    feat: add test for empty array

--- src/test/server.test.ts
+++ src/test/server.test.ts
@@ -1,7 +1,11 @@
 import request from 'supertest';
 import { server } from '../index';
+import { IUser } from '../interface/User';
+import { ICodes } from '../interface/Codes';
 
 describe('Server', () => {
+  const users: IUser[] = [];
+
   test('should respond with status code 200', async () => {
     const response = await request(server).get('/api/users');
     expect(response.status).toBe(200);
@@ -14,5 +18,16 @@ describe('Server', () => {
   test('should respond with JSON content type', async () => {
     const response = await request(server).get('/api/users');
     expect(response.headers['content-type']).toMatch(/json/);
+  });
+
+  test('should return empty array', async () => {
+    const response = await request(server)
+      .get('/api/users')
+      .set('Accept', 'application/json');
+
+    expect(response.statusCode).toEqual(ICodes.OK);
+    expect(response.headers['content-type']).toEqual('application/json');
+    expect(response.body).toBeDefined();
+    expect(response.body.length).toEqual(users.length);
   });
 });
